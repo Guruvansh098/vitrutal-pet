2e4095bba75fa9a5ce18a525e4afe606c986abde
Guruvansh098 <70893637+Guruvansh098@users.noreply.github.com>
2020-11-21 13:22:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+
+
+var dog,happydog,database,foodS,foodstock
+var dogimage,happydogimage
+
+function preload()
+{
+  dogimage=loadImage("images/Dog.png")
+	
+}
+
+function setup() {
+  createCanvas(500,500);
+  background(46,139,87)
+  var dog=createSprite(100,100)
+  dog.setAnimation("")
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
